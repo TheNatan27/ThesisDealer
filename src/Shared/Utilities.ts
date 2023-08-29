e0262f5ed0151c2abe2783b52021dda347455936
TheNatan27 <74319000+TheNatan27@users.noreply.github.com>
2023-08-29 10:37:35 +0200

    Revorked test reservation process

--- src/Shared/Utilities.ts
+++ src/Shared/Utilities.ts
@@ -1,7 +1,7 @@
 import {ITestClass, QueryTestClass} from '../Repository/TestClass';
 import PostgresConnector from './PostgresConnector';
+import {TestObjectType} from './TestClassTypes';
 
-export async function processResults(testClass: ITestClass, uuid: string) {
-  const data = new QueryTestClass({testClass, suiteId: uuid});
-  await PostgresConnector.getInstance().insertTestResult(data);
+export async function processResults(completedTest: TestObjectType) {
+  await PostgresConnector.getInstance().insertTestResult(completedTest);
 }
