ccecb9496f1a318641fd6c673ed8e7e34ce79198
TheNatan27 <74319000+TheNatan27@users.noreply.github.com>
2023-09-18 17:48:25 +0200

    Change docker deployment commands

--- src/Shared/DockerConnector.ts
+++ src/Shared/DockerConnector.ts
@@ -3,12 +3,23 @@ import dotenv from 'dotenv';
 
 async function createDeployment(suiteId: string, dockerId: string) {
   dotenv.config();
-  const ipAddresss = process.env.IP_ADDRESS;
-  const replicas = process.env.REPLICAS;
+  const ipAddresss = process.env.IP_ADDRESS!;
+  const replicas = process.env.REPLICAS!;
   const parameters = `docker service create --env SUITE_ID=${suiteId} --env IP_ADDRESS=${ipAddresss} --name ${dockerId} --replicas ${replicas} worker-image:latest`;
 
   try {
-    await execa(parameters);
+    await execa('docker', [
+      'service',
+      'create',
+      '--env',
+      `SUITE_ID=${suiteId}`,
+      `--env IP_ADDRESS=${ipAddresss}`,
+      '--name',
+      dockerId,
+      '--replicas',
+      replicas,
+      'merninfo/worker-image:latest',
+    ]);
   } catch (error) {
     console.error(error);
     //TODO throw deployment error
@@ -18,7 +29,7 @@ async function createDeployment(suiteId: string, dockerId: string) {
 async function removeDeployment(dockerId: string) {
   console.log(`Remove docker service: ${dockerId}`);
   try {
-    const {stdout} = await execa(`docker service rm ${dockerId}`);
+    const {stdout} = await execa('docker', ['service', 'rm', dockerId]);
     console.log(stdout);
   } catch (error) {
     console.error(error);
